b0ce30d33369b74c5d56bbe4f948355464e54281
Caio Reis <caiodtn@gmail.com>
2025-03-30 14:35:30 -0300

    back-end now search mock

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,7 @@
+import csv
+import os
+from datetime import datetime
+
 from fastapi import FastAPI, HTTPException
 from src.database.main import SessionLocal
 from src.database.schemas import OperadoraBase
@@ -8,10 +12,11 @@ from src.models.operadoras import Operadora
 
 app = FastAPI(
     title="Intuitive Care API",
-    description="API to manage to GET Data from Intuitive Care Database",
+    description="API to GET Orgs",
     version="0.1.0",
     contact={
         "name": "Caio Reis",
+        "email": "caiodtn@gmail.com"
     }
 )
 
@@ -71,4 +76,48 @@ async def say_hello(name: str = None):
                 )
             )
     db.close()
+    return response
+
+
+@app.get("/search_mock", response_model_exclude_none=True, response_model=list[Operadora])
+async def search_mock(name: str = None):
+    if not name:
+        raise HTTPException(status_code=400, detail="Name is required")
+
+    file_path = os.path.join("src", "mock", "Relatorio_cadop.csv")
+    response: list[Operadora] = []
+
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            razao_social = row[2]
+            nome_fantasia = row[3]
+            if name.lower() in razao_social.lower() or name.lower() in nome_fantasia.lower():
+                response.append(
+                    Operadora(
+                        id=int(row[0]),
+                        registro_ans=row[1],
+                        cnpj=row[1],
+                        razao_social=razao_social,
+                        nome_fantasia=nome_fantasia,
+                        modalidade=row[4],
+                        logradouro=row[5],
+                        numero=row[6],
+                        complemento=row[7],
+                        bairro=row[8],
+                        cidade=row[9],
+                        uf=row[10],
+                        cep=row[11],
+                        telefone=f"{row[12]} {row[13]}",
+                        email=row[15],
+                        representante_legal=row[16],
+                        cargo_representante=row[17],
+                        regiao_operacao=int(row[18]),
+                        data_registro=datetime.strptime(row[19], "%d/%m/%Y").date() if row[19] else None,
+                    )
+                )
+
+    if not response:
+        raise HTTPException(status_code=404, detail="No organization found")
+
     return response
